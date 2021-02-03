0623d16df65b7f7dcd32e0feeceda82d072b618f
frog <frog@pop-os.localdomain>
2021-02-03 16:09:48 +0000

    fixed spelling issue

--- init.el
+++ init.el
@@ -6,19 +6,4 @@
 (org-babel-load-file "~/.emacs.d/config/functions.org")
 (org-babel-load-file "~/.emacs.d/config/major-modes.org")
 (org-babel-load-file "~/.emacs.d/config/yas.org")
-(org-babel-load-file "~/.emacs.d/config/hepful-packages.org")
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (vterm flycheck arduino-mode evil-collection evil exwm use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(org-babel-load-file "~/.emacs.d/config/helpful-packages.org")
