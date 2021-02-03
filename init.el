0fb80026ae1a42706ccf387e0633b99a649fd523
frog <frog@pop-os.localdomain>
2021-02-03 16:51:38 +0000

    magit

--- init.el
+++ init.el
@@ -7,3 +7,19 @@
 (org-babel-load-file "~/.emacs.d/config/major-modes.org")
 (org-babel-load-file "~/.emacs.d/config/yas.org")
 (org-babel-load-file "~/.emacs.d/config/helpful-packages.org")
+(org-babel-load-file "~/.emacs.d/config/magit.org")
+(org-babel-load-file "~/.emacs.d/config/projectile.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (company helpful which-key yasnippet web-mode typescript-mode slime cider clojure-mode haskell-mode rust-mode csharp-mode org-bullets rainbow-delimiters doom-themes doom-modeline all-the-icons ivy-rich counsel ivy diminish evil-collection evil exec-path-from-shell use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
